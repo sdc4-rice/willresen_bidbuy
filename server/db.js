81a66fac524fee84aaef52271ea7ae11b08ed5f7
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-03 18:46:02 -0400

    Set 'useCreateIndex' to true Avoids deprecation warning. See https://mongoosejs.com/docs/deprecations.html for more information.

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/products-bid-buy', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/products-bid-buy', {useNewUrlParser: true, useCreateIndex: true});
 
 const Product = mongoose.model('Product', {
   id: {
