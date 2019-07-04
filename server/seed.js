8ae848314e5e2d57f751eed62dde0d785eb4299b
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-03 21:31:38 -0400

    Edit comments for clarity

--- server/seed.js
+++ server/seed.js
@@ -13,14 +13,14 @@ const sellerNote = () => {
 };
 
 // Returns a URL-friendly version of the given string. For example, 'John O'Riley' => 'john-oriley'.
-// This function will help produce names that can be more easily used in API requests.
+// This function helps produce names that can be used in API requests.
 const urlify = (string) => {
   return string.toLowerCase()
     .replace(/\s/g, '-')
-    .replace(/[^a-zA-Z\-\d]/g, '');
+    .replace(/[^a-zA-Z\-\d]/g, ''); // strip out any character that is not a letter, digit, or hyphen
 };
 
-// Roughly 80% of the productes should be returnable
+// Roughly 80% of the products should be returnable
 const returnsAllowed = () => {
   return Math.random() < 0.8;
 };
@@ -45,14 +45,13 @@ const generateProduct = (id) => {
 };
 
 // MAIN FUNCTION
-// This function generates fake produces and adds them to the database. It returns
+// This function generates fake products and adds them to the database. It returns
 // a promise that resolves once all fake products have been added to the database.
 const generate = (startId, endId) => {
   console.log('Seeding database...');
   console.log(`Adding items with ids ${startId} to ${endId}`);
 
   const fakeProducts = [];
-
   for (let i = startId; i <= endId; i++) {
     fakeProducts.push(generateProduct(i));
   }
@@ -61,8 +60,8 @@ const generate = (startId, endId) => {
 };
 
 // This function automatically runs when this file is run. Configure `startId`
-// and `endId` as desired. Note: This drops the existing collection before adding
-// new data to it.
+// and `endId` as desired. Note: This function drops the existing collection
+// before adding new data to it.
 const seedDatabase = (() => {
   const startId = 100;
   const endId = 110;
