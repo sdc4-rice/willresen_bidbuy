00ac0b311d7384608dcc5b42f1f82ee8708b7e82
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-04 12:13:28 -0400

    Fix typo in comment

--- server/seed.js
+++ server/seed.js
@@ -1,6 +1,6 @@
 // This file is run with `npm run seed`.
 //
-// When thils file is run, the `handleSeeding` function is executed and the
+// When this file is run, the `handleSeeding` function is executed and the
 // database is seeded with products. You can optionally pass in `startId` and
 // `endId` integer arguments to `handleSeeding`.
 
