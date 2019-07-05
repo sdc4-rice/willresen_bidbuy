ee151808cdaf20e8e859fd0ae12fc01ae431537d
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-05 11:59:19 -0400

    Implement bid API POST endpoint

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const { Product } = require('./db.js');
 const port = 3001;
 const app = express();
 
+app.use(express.json());
+
 app.get('/items/id/:id', (req, res) => {
   const id = req.params.id;
 
@@ -18,6 +20,28 @@ app.get('/items/name/:name', (req, res) => {
   Product.find({'url-name': name})
     .then(product => res.json(product))
     .catch(err => res.json(err));
+});
+
+app.post('/bid/:id', (req, res) => {
+  const id = req.params.id;
+  const bid = req.body.bid;
+
+  const validateBid = (price) => {
+    if (price < bid) {
+      return true;
+    }
+    throw 'Bid too low';
+  };
+
+  Product.findOne({id})
+    .then(product => validateBid(product.price))
+    .then(() => Product.findOneAndUpdate({id}, {price: bid}))
+    .then(newProduct => res.json(newProduct))
+    .catch(err => res.json({error: true, message: err}));
+
+  // Product.findOneAndUpdate({id}, {price})
+  //   .then(dbResponse => res.json(dbResponse))
+  //   .catch(err => res.json(err));
 });
 
 app.listen(port, () => {
