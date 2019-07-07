492ad9643e335250dfa672ff9de16468e86722c2
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-07 15:16:35 -0400

    Remove unused variable

--- server/db.js
+++ server/db.js
@@ -5,7 +5,6 @@ const database = 'products-bid-buy';
 
 const handleConnect = () => {
   if (mongoose.connection.readyState === 0) { // not connected to database
-    const dbName = 'products-bid-buy';
     return mongoose.connect(`mongodb://localhost:27017/${database}`, {useNewUrlParser: true, useCreateIndex: true});
   }
   return Promise.resolve(false); // already connected
