66d9ba2653a4fab2e4a032376cfe72ce37aa5385
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-07 13:39:00 -0400

    Make 'seed.js' exit when done seeding

--- server/seed.js
+++ server/seed.js
@@ -3,7 +3,13 @@
 // When this file is run, the `handleSeeding` function is executed and the
 // database is seeded with products. You can optionally pass in `startId` and
 // `endId` integer arguments to `handleSeeding`.
+//
+// This file exists automatically when the seeding script is done. The exit
+// code indicates whether the seeding was successful.
 
+const process = require('process');
 const { handleSeeding } = require('./seedHelpers.js');
 
-handleSeeding();
+handleSeeding()
+  .then(() => process.exit(0))
+  .catch(() => process.exit(1));
