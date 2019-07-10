c9876209e2f2df21fe0b5da12a93a12791a510c8
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-10 14:43:31 -0400

    Refactor snapshot tests

--- __tests__/client/TopInfo.js
+++ __tests__/client/TopInfo.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import TopInfo from '../../client/TopInfo.js';
 import renderer from 'react-test-renderer';
-
-const placeholderProduct = {
-  id: 109,
-  name: 'Fantastic Concrete Fish',
-  'url-name': 'fantastic-concrete-fish',
-  condition: 'New',
-  price: 564,
-  sellerNote: 'Numquam eius laborum.',
-  expiresAt: '2019-07-28T18:05:33.973Z',
-  watchers: 26,
-  bids: 37,
-  shippingCountry: 'Eritrea',
-  returnsAllowed: true,
-};
+import placeholderProduct from './placeholderProduct.js';
 
 it('renders correctly', () => {
   const tree = renderer
