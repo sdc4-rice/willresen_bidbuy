c623ae19118f16858ffdd1ad0642599055b3de54
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-11 09:23:01 -0400

    Fix BidInfo test

--- __tests__/client/BidInfo.enzyme.js
+++ __tests__/client/BidInfo.enzyme.js
@@ -6,8 +6,7 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-xtest('has a bid button', () => { // currently not working
-  const component = render(<TopInfo product={placeholderProduct} />);
-  console.log(component);
-  expect(component.find('form').length).toBe(1);
+test('has a tbody element', () => {
+  const component = shallow(<TopInfo product={placeholderProduct} />);
+  expect(component.find('tbody').length).toBe(1);
 });
