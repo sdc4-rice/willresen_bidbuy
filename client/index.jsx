45d1fc507af07f8d0e263fbee298c961bd107b3b
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-15 17:32:31 -0400

    Rename main app HTML element

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BidBuy from './BidBuy';
 
-ReactDOM.render(<BidBuy />, document.getElementById('app'));
+ReactDOM.render(<BidBuy />, document.getElementById('bid-buy'));
