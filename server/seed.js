3c2a06a223092a4c38cad3773e390f14ad88c5df
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-20 19:11:08 -0400

    Correct outdated comment

--- server/seed.js
+++ server/seed.js
@@ -1,13 +1,11 @@
 // This file is run with `npm run seed`.
 //
 // When this file is run, the `handleSeeding` function is executed and the
-// database is seeded with products. You can optionally pass in `startId` and
-// `endId` integer arguments to `handleSeeding`.
+// database is seeded with products.
 //
 // This file exits automatically when the seeding script is done. The exit
 // code indicates whether the seeding was successful.
 
-const process = require('process');
 const { handleSeeding } = require('./seedHelpers.js');
 
 handleSeeding()
