fc6c7624bcba3c11a013a0a1f54f0bfcbd680ae0
Will Resen <willresen@gmail.com>
2019-07-23 20:30:16 -0700

    Remove Mongoose connection

--- server/db.js
+++ server/db.js
@@ -1,16 +1,19 @@
-// const mongoose = require('mongoose');
-// mongoose.Promise = require('bluebird');
-// require('dotenv').config();
+const Sequelize = require('sequelize');
 
-// const databaseName = process.env.DB_NAME;
-// const databaseHost = process.env.DB_HOST || 'localhost';
+const sequelize = new Sequelize('postgres://postgres@localhost:5432/bidbuy');
 
-// const handleConnect = () => {
-//   if (mongoose.connection.readyState === 0) { // not connected to database
-//     return mongoose.connect(`mongodb://${databaseHost}:27017/${databaseName}`, { useNewUrlParser: true, useCreateIndex: true });
-//   }
-//   return Promise.resolve(false); // already connected
-// };
+
+
+
+
+
+
+
+
+
+
+
+
 
 // module.exports = {
 //   databaseName,
