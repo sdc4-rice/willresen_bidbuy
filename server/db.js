ddd242095476122684f48fad6f0a9c37fd011b01
Will Resen <willresen@gmail.com>
2019-07-23 20:22:34 -0700

    Prep database for migration to PostgreSQL

--- server/db.js
+++ server/db.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
-require('dotenv').config();
+// const mongoose = require('mongoose');
+// mongoose.Promise = require('bluebird');
+// require('dotenv').config();
 
-const databaseName = process.env.DB_NAME;
-const databaseHost = process.env.DB_HOST || 'localhost';
+// const databaseName = process.env.DB_NAME;
+// const databaseHost = process.env.DB_HOST || 'localhost';
 
-const handleConnect = () => {
-  if (mongoose.connection.readyState === 0) { // not connected to database
-    return mongoose.connect(`mongodb://${databaseHost}:27017/${databaseName}`, { useNewUrlParser: true, useCreateIndex: true });
-  }
-  return Promise.resolve(false); // already connected
-};
+// const handleConnect = () => {
+//   if (mongoose.connection.readyState === 0) { // not connected to database
+//     return mongoose.connect(`mongodb://${databaseHost}:27017/${databaseName}`, { useNewUrlParser: true, useCreateIndex: true });
+//   }
+//   return Promise.resolve(false); // already connected
+// };
 
-module.exports = {
-  databaseName,
-  mongoose,
-  handleConnect,
-};
+// module.exports = {
+//   databaseName,
+//   mongoose,
+//   handleConnect,
+// };
