d21758f02c69fac3a9c90ea48fd03d7d386cdbdc
Will Resen <willresen@gmail.com>
2019-07-23 20:59:20 -0700

    Implement PostgreSQL schema for Products

--- server/db.js
+++ server/db.js
@@ -1,8 +1,36 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('postgres://postgres@localhost:5432/bidbuy');
+const Model = Sequelize.Model;
+
+class Product extends Model {};
+
+Product.init({
+  id: {
+    type: Sequelize.DECIMAL,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  url: Sequelize.STRING,
+  condition: Sequelize.STRING,
+  price: Sequelize.DECIMAL,
+  sellerNote: Sequelize.STRING,
+  expiresAt: Sequelize.DATE,
+  watchers: Sequelize.DECIMAL,
+  bids: Sequelize.DECIMAL,
+  shippingCountry: Sequelize.STRING,
+  returnsAllowed: Sequelize.BOOLEAN,
+}, { sequelize, modelName: 'product' });
 
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 
 
