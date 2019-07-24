76e2d91b457e7d77f03743b1dd99f0927e6da8d7
Will Resen <willresen@gmail.com>
2019-07-23 20:22:10 -0700

    Strip Mongoose/MongoDB from API

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db.js');
-const Product = require('./model.js');
 require('dotenv').config();
 
 const port = process.env.PORT;
@@ -10,28 +9,23 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cors());
-db.handleConnect();
 
+//retrieve all products from database
 app.get('/items', (req, res) => {
-  Product.find()
-    .then(products => res.json(products))
-    .catch(err => res.json(err));
+
 });
 
+//find product by id
 app.get('/items/id/:id', (req, res) => {
   const { id } = req.params;
 
-  Product.findOne({ id })
-    .then(product => res.json(product))
-    .catch(err => res.json(err));
 });
 
+ //find product by name
 app.get('/items/name/:name', (req, res) => {
   const { name } = req.params;
 
-  Product.findOne({ 'url-name': name })
-    .then(product => res.json(product))
-    .catch(err => res.json(err));
+
 });
 
 app.post('/bid/:id', (req, res) => {
@@ -45,13 +39,14 @@ app.post('/bid/:id', (req, res) => {
     throw Error(`Bid of $${bid} is not greater than $${price}.`);
   };
 
-  Product.findOne({ id })
-    .then(product => product) // This shouldn't be necessary, but I get an error if I omit it
-    .tap(product => validateBid(product.price))
-    .then(product => product.bids + 1)
-    .then(bids => Product.findOneAndUpdate({ id }, { price: bid, bids }, { new: true }))
-    .then(updatedProduct => res.json(updatedProduct))
-    .catch(err => res.json({ error: true, message: err.message }));
+  // find a product by id
+  // validate that its current price < bid price
+  // if it is, add one to the number of bids. if not, make sure to use .catch
+          // and send "res.json({ error: true, message: err.message });"
+
+  // update the database entry with new number of bids and new price
+  // return updated database entry to client
+
 });
 
 app.listen(port, () => {
