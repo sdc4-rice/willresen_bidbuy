20e63f94354e090519b004f86ab559e4f0ce5265
Will Resen <willresen@gmail.com>
2019-07-26 14:36:06 -0700

    Disable sequelize logging

--- server/postgres.js
+++ server/postgres.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres://postgres@localhost:5432/bidbuy');
+const sequelize = new Sequelize('postgres://postgres@localhost:5432/bidbuy', {logging: false});
 const Model = Sequelize.Model;
 
 class Item extends Model {};
