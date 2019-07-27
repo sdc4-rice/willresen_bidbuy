571243a125cb7e13c9d16013015129a1e0ceb5b8
Will Resen <willresen@gmail.com>
2019-07-27 14:56:22 -0700

    Refactor seeder - 1M rows in 21 seconds

--- server/seed.js
+++ server/seed.js
@@ -1,14 +1,95 @@
 require('dotenv').config();
-// This file is run with `npm run seed`.
-//
-// When this file is run, the `handleSeeding` function is executed and the
-// database is seeded with products.
-//
-// This file exits automatically when the seeding script is done. The exit
-// code indicates whether the seeding was successful.
+const faker = require('faker');
+const fs = require('fs');
+const util = require('util')
+const exec = util.promisify(require('child_process').exec);
+const db = require(`./${process.env.DATABASE}.js`);
 
-const { handleSeeding } = require(`./${process.env.DATABASE}_seedHelpers.js`);
 
-handleSeeding()
-  .then(() => process.exit(0))
-  .catch(() => process.exit(1));
+// HELPER FUNCTIONS
+const randomCondition = () => {
+  const conditions = ['New', 'Mint', 'Used', 'Fair'];
+  return conditions[faker.random.number(3)];
+};
+
+// Roughly half the products should have notes from the seller
+const sellerNote = () => (Math.random() > 0.5 ? faker.lorem.sentence() : '');
+
+// Returns a URL-friendly version of the given string. For example,
+// 'John O'Riley' => 'john-oriley'.
+// This function helps produce names that can be used in API requests.
+const urlify = string => string.toLowerCase()
+  .replace(/\s/g, '-')
+  .replace(/[^a-zA-Z\-\d]/g, ''); // strip out any character that is not a letter, digit, or hyphen
+
+// Roughly 80% of the products should be returnable
+const returnsAllowed = () => Math.random() < 0.8;
+
+// Returns an object representing a fake product
+const generateProduct = () => {
+  const name = faker.commerce.productName();;
+
+  return {
+    id: faker.random.alphaNumeric(15),
+    name,
+    url: urlify(name) + '-' + faker.random.alphaNumeric(3),
+    condition: randomCondition(),
+    price: parseFloat(faker.commerce.price()),
+    sellerNote: sellerNote(),
+    expiresAt: faker.date.future(1).toUTCString().replace(',', ''),
+    createdAt: faker.date.recent(-90).toUTCString().replace(',', ''),
+    watchers: faker.random.number(75),
+    bids: faker.random.number(50),
+    shippingCountry: faker.address.country().replace(',', ''),
+    returnsAllowed: returnsAllowed(),
+  };
+};
+
+let stream = fs.createWriteStream(__dirname + '/mockData.csv');
+
+const generateRows = async (rows) => {
+  console.log('Starting to generate...');
+  console.time('Seeder');
+  db.sequelize.authenticate().then(() => db.sequelize.sync({force: true})).catch(err => console.log(err))
+
+  const write = async () => {
+    let ok = true;
+    while (ok && rows > 0) {
+      ok = await stream.write(Object.values(generateProduct()).join(',') + '\r\n', err => err && console.log(err));
+      rows--;
+    }
+    console.log('Remaining: ' + rows);
+    if (rows > 0) {
+      return new Promise((resolve, reject) => stream.once('drain', () => {
+        resolve(write());
+      }))
+    } else {
+      return Promise.resolve(true);
+    }
+  }
+  await write();
+  console.log('Done writing!');
+  return Promise.resolve(true);
+};
+
+generateRows(process.env.ROWS_TO_GENERATE) //define the number of rows to generate
+  /* Transfer the generated CSV to the docker container */
+  .then(async () => {
+    console.log('>>> Transferring to Docker, please wait... <<<');
+    const { stdout, stderr } = await exec(`docker cp ${__dirname}/mockData.csv postgres:/mockData.csv`);
+    if (stdout) console.log('stdout:', stdout);
+    if (stderr) throw stderr;
+  })
+  /* Import the CSV to the database */
+  .then(async () => {
+    console.log('>>> Importing to Postgres, please wait... <<<');
+    const { stdout, stderr } = await exec(`docker exec postgres psql -U postgres bidbuy -c "\\copy items from mockData.csv with (format 'csv');"`);
+    if (stdout) console.log('stdout:', stdout);
+    if (stderr) throw stderr;
+  })
+  .then(() => console.timeEnd('Seeder'))
+  .then(() => process.exit(0));
+
+module.exports = {
+  generateProduct,
+};
