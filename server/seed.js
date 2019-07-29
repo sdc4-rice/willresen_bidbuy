4356ef5b26a7d4251cf46543c648275703de27aa
Will Resen <willresen@gmail.com>
2019-07-28 20:54:39 -0700

    Implement CSV seeding and import to both Cassandra and Postgres based on env vars

--- server/seed.js
+++ server/seed.js
@@ -3,7 +3,8 @@ const faker = require('faker');
 const fs = require('fs');
 const util = require('util')
 const exec = util.promisify(require('child_process').exec);
-const db = require(`./${process.env.DATABASE}.js`);
+const currentDb = process.env.DATABASE;
+const db = require(`./${currentDb}.js`);
 
 
 // HELPER FUNCTIONS
@@ -26,18 +27,18 @@ const urlify = string => string.toLowerCase()
 const returnsAllowed = () => Math.random() < 0.8;
 
 // Returns an object representing a fake product
-const generateProduct = () => {
+const generateProduct = (rows) => {
   const name = faker.commerce.productName();;
 
   return {
-    id: faker.random.alphaNumeric(15),
+    id: rows,
     name,
     url: urlify(name) + '-' + faker.random.alphaNumeric(3),
     condition: randomCondition(),
     price: parseFloat(faker.commerce.price()),
     sellerNote: sellerNote(),
-    expiresAt: faker.date.future(1).toUTCString().replace(',', ''),
-    createdAt: faker.date.recent(-90).toUTCString().replace(',', ''),
+    expiresAt: faker.date.future(1).toJSON().replace(',', ''),
+    createdAt: faker.date.recent(-90).toJSON().replace(',', ''),
     watchers: faker.random.number(75),
     bids: faker.random.number(50),
     shippingCountry: faker.address.country().replace(',', ''),
@@ -50,42 +51,62 @@ let stream = fs.createWriteStream(__dirname + '/mockData.csv');
 const generateRows = async (rows) => {
   console.log('Starting to generate...');
   console.time('Seeder');
-  /* Verify that the PostgreSQL db is running */
-  db.sequelize.authenticate()
-    .then(() => db.sequelize.sync({ force: true }))
-    .catch(() => process.exit(0))
+
+  /* Verify that the database is running and drop existing table */
+  if (currentDb === 'postgres') {
+    db.sequelize.authenticate()
+      .then(() => db.sequelize.sync({ force: true }))
+      .catch(() => process.exit(0))
+  } else if (currentDb === 'cassandra') {
+    db.client.connect()
+      .then(() => db.client.execute('DROP TABLE IF EXISTS items'))
+      .then(() => db.client.execute(db.Items))
+      .catch(() => process.exit(0))
+  }
 
   const write = async () => {
     let ok = true;
     while (ok && rows > 0) {
-      ok = await stream.write(Object.values(generateProduct()).join(',') + '\r\n', err => err && console.log(err));
+      ok = await stream.write(Object.values(generateProduct(rows)).join(',') + '\r\n', err => err && console.log(err));
       rows--;
     }
     console.log(rows + ' rows remaining.');
     return rows > 0 ? new Promise((resolve, reject) => stream.once('drain', () => resolve(write() ))) : Promise.resolve(true);
+    //is there a better way to write this line? ^^^ The stream.once callback executes immediately - but we want it to wait.
   }
   await write();
   console.log('Done writing!');
   return Promise.resolve(true);
 };
 
-generateRows(process.env.ROWS_TO_GENERATE) //define the number of rows to generate
+generateRows(process.env.ROWS_TO_GENERATE)
+
   /* Transfer the generated CSV to the docker container */
   .then(async () => {
     console.log('>>> Transferring to Docker, please wait... <<<');
-    const { stdout, stderr } = await exec(`docker cp ${__dirname}/mockData.csv postgres:/mockData.csv`);
+    const { stdout, stderr } = await exec(`docker cp ${__dirname}/mockData.csv ${currentDb}:/mockData.csv`);
     if (stdout) console.log('stdout:', stdout);
     if (stderr) throw stderr;
   })
+
   /* Import the CSV to the database */
   .then(async () => {
-    console.log('>>> Importing to Postgres, please wait... <<<');
-    const { stdout, stderr } = await exec(`docker exec postgres psql -U postgres bidbuy -c "\\copy items from mockData.csv with (format 'csv');"`);
-    if (stdout) console.log('stdout:', stdout);
-    if (stderr) throw stderr;
+    if (currentDb === 'postgres') {
+      console.log('>>> Importing to Postgres, please wait... <<<');
+      const { stdout, stderr } = await exec(`docker exec postgres psql -U postgres bidbuy -c "\\copy items from mockData.csv with (format 'csv');"`);
+      if (stdout) console.log('stdout:', stdout);
+      if (stderr) throw stderr;
+
+    } else if (currentDb === 'cassandra') {
+      const { stdout, stderr } = await exec(`docker exec cassandra cqlsh localhost -k bidbuy -e "copy items (id, name, url, condition, price, sellerNote, expiresAt, createdAt, watchers, bids, shippingCountry, returnsAllowed) from 'mockData.csv'"`);
+      if (stdout) console.log('stdout:', stdout);
+      if (stderr) throw stderr;
+    }
+
   })
   .then(() => console.timeEnd('Seeder'))
   .then(() => process.exit(0));
+
 
 module.exports = {
   generateProduct,
