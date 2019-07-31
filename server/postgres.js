be62f482e37aaf5d351f55beb3dbf75649adf7d9
Will Resen <willresen@gmail.com>
2019-07-31 12:13:26 -0700

    Update Item schema

--- server/postgres.js
+++ server/postgres.js
@@ -3,10 +3,10 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('postgres://postgres@localhost:5432/bidbuy', {
   logging: false,
   pool: {
-    max: 10,
+    max: 20,
     min: 5,
-    idle: 100000
-  }
+    idle: 1000
+  },
 });
 const Model = Sequelize.Model;
 
@@ -16,18 +16,19 @@ Item.init({
   id: {
     type: Sequelize.INTEGER,
     primaryKey: true,
+    autoIncrement: true
   },
   name: Sequelize.STRING,
   url: Sequelize.STRING,
   condition: Sequelize.STRING,
   price: Sequelize.DECIMAL,
-  sellerNote: Sequelize.STRING,
-  expiresAt: Sequelize.DATE,
-  createdAt: Sequelize.DATE,
+  sellernote: Sequelize.STRING,
+  expiresat: Sequelize.DATE,
+  createdat: Sequelize.DATE,
   watchers: Sequelize.INTEGER,
   bids: Sequelize.INTEGER,
-  shippingCountry: Sequelize.STRING,
-  returnsAllowed: Sequelize.BOOLEAN,
+  shippingcountry: Sequelize.STRING,
+  returnsallowed: Sequelize.BOOLEAN,
 }, { sequelize, modelName: 'item', timestamps: false });
 
 
