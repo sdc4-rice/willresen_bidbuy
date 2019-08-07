c963acb4591dfa77722f22eaf26ea2be97ef4921
Will Resen <willresen@gmail.com>
2019-08-06 19:24:20 -0700

    Implement dynamic server-side rendering

--- server/index.js
+++ server/index.js
@@ -13,25 +13,26 @@ const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(express.static('public'));
 app.use(cors());
 
-const handleRender = async (req, res) => {
-  await db.getById(req.query.id)
+const handleRender = (req, res) => {
+  db.getById(req.query.id)
     .then(results => {
+      /* Generate HTML for the stylized-component */
       const sheet = new ServerStyleSheet();
       const html = ReactDOMServer.renderToString(sheet.collectStyles(React.createElement(BidBuy.default, { product: results })));
       const styleTags = sheet.getStyleTags();
       sheet.seal();
-      fs.readFile('./public/index.html', 'utf8', (err, data) => {
+      /* Get HTML from the template and insert the component's HTML */
+      fs.readFile('./public/template.html', 'utf8', (err, data) => {
         if (err) throw err;
-        const document = data.replace(/<div id="bid-buy"><\/div>/, `<div id="bid-buy">${html}</div>`)
+        const document = data.replace(/<div id="bid-buy"><\/div>/, `<div id="bid-buy">${html}</div><script src="bundle.js"></script>`)
           .replace(/<\/head>/, `${styleTags}</head>`);
         res.send(document);
       });
     })
 };
-
-// app.use(express.static('public'));
 
 app.get('/', handleRender);
 
